81a7bb143b1819718a9dab968b57130a05791bb8
davidtb10 <50488430+davidtb10@users.noreply.github.com>
2021-05-18 13:57:44 +0200

    Modificado carousel para aumentar el tiempo y que se pueda pausar y continuar

--- Practica3/javascript/index.js
+++ Practica3/javascript/index.js
@@ -21,3 +21,21 @@ annotation4.show();
 const e = document.querySelector('.list');
 const annotation5 = annotate(e, { type: 'bracket', brackets: ['left'], color: 'crimson', padding: [2,5]});
 annotation5.show();
+
+function pauseCarousel() {
+    $('#control-pause').hide();
+    $('#control-play').show();
+    $('.carousel').carousel('pause');
+}
+
+function playCarousel() {
+    $('#control-play').hide();
+    $('#control-pause').show();
+    $('.carousel').carousel('cycle');
+}
+
+$(function() {
+    $("#control-play").hide();
+    $("#control-pause").click(pauseCarousel);
+    $("#control-play").click(playCarousel);
+});
