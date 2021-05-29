bbcb2721efeab50b0722f38fa0dedc510cd73b93
davidtb10 <50488430+davidtb10@users.noreply.github.com>
2021-05-29 14:21:54 +0200

    Mejorada la validacion de errores del formulario

--- Practica3/javascript/formulario.js
+++ Practica3/javascript/formulario.js
@@ -1,3 +1,6 @@
+//Array para almacenar los inputs con errores
+var camposErrores = new Array();
+
 $(document).ready(function (e) {
 	$('form').submit(function () {
 		//Crear variable para controlar si hay errores
@@ -6,14 +9,19 @@ $(document).ready(function (e) {
 		//Reiniciar mensajes de error
 		$('#errors').html('');
 
+		//Limpiar campos
+		limpiarCampos();
+
 		//Validar nombre
 		if ($('#inputNombre').val() === '') {
 			errors = true;
+			camposErrores.push('#inputNombre');
 			$('#errors').append('<p><strong>El campo nombre es obligatorio.</strong></p>');
 		} else {
 			var pattern = /^[a-zA-ZñÑ\s\W]+$/;
 			if (!$("#inputNombre").val().match(pattern)) {
 				errors = true;
+				camposErrores.push('#inputNombre');
 				$('#errors').append('<p><strong>El nombre únicamente puede contener caracteres no numéricos.</strong></p>');
 			}
 		}
@@ -21,11 +29,13 @@ $(document).ready(function (e) {
 		//Validar apellidos
 		if ($('#inputApellidos').val() === '') {
 			errors = true;
+			camposErrores.push('#inputApellidos');
 			$('#errors').append('<p><strong>El campo apellidos es obligatorio.</strong></p>');
 		} else {
 			var pattern = /^[a-zA-ZñÑ\s\W]+$/;
 			if (!$("#inputApellidos").val().match(pattern)) {
 				errors = true;
+				camposErrores.push('#inputApellidos');
 				$('#errors').append('<p><strong>Los apellidos únicamente pueden contener caracteres no numéricos.</strong></p>');
 			}
 		}
@@ -36,12 +46,18 @@ $(document).ready(function (e) {
 		var anio = $('#inputAnio').val();
 		if (dia === '' || mes === '' || anio === '') {
 			errors = true;
+			camposErrores.push('#inputDia');
+			camposErrores.push('#inputMes');
+			camposErrores.push('#inputAnio');
 			$('#errors').append('<p><strong>El campo fecha de nacimiento es obligatorio.</strong></p>');
 		} else {
 			var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
 			var fecha = dia + '/' + mes + '/' + anio;
 			if (!fecha.match(pattern)) {
 				errors = true;
+				camposErrores.push('#inputDia');
+				camposErrores.push('#inputMes');
+				camposErrores.push('#inputAnio');
 				$('#errors').append('<p><strong>La fecha de nacimiento es incorrecta, debe seguir el formato (dd/mm/aaaa).</strong></p>');
 			}
 		}
@@ -49,6 +65,7 @@ $(document).ready(function (e) {
 		//Validar email
 		if ($('#inputEmail').val() === '') {
 			errors = true;
+			camposErrores.push('#inputEmail');
 			$('#errors').append('<p><strong>El campo email es obligatorio.</strong></p>');
 		}
 
@@ -58,6 +75,7 @@ $(document).ready(function (e) {
 		if (telefono.length != 0) {
 			if (!telefono.match(pattern)) {
 				errors = true;
+				camposErrores.push('#inputTelefono');
 				$('#errors').append('<p><strong>El formato del teléfono es incorrecto, únicamente puede contener dígitos.</strong></p>');
 			}
 		}
@@ -79,6 +97,7 @@ $(document).ready(function (e) {
 		if (pais.length != 0) {
 			if (!pais.match(pattern)) {
 				errors = true;
+				camposErrores.push('#inputPais');
 				$('#errors').append('<p><strong>El país es incorrecto, únicamente puede incluir caracteres no numéricos.</strong></p>');
 			}
 		}
@@ -89,11 +108,15 @@ $(document).ready(function (e) {
 		if (codigo.length != 0) {
 			if (!codigo.match(pattern)) {
 				errors = true;
+				camposErrores.push('#inputcodigo');
 				$('#errors').append('<p><strong>El código postal es incorrecto, debe estar formado por 5 dígitos.</strong></p>');
 			}
 		}
-
-		if(!errors){
+		
+		if(errors){
+			marcarCamposConErrores();
+			$(window).scrollTop(0);
+		}else{
 			window.location.href = "index.html";
 		}
 
@@ -101,51 +124,18 @@ $(document).ready(function (e) {
 	});
 });
 
+//Se marcan visualmente los campos con errores
+function marcarCamposConErrores(){
+	camposErrores.forEach(function(campo, index) {
+		$(campo).addClass("is-invalid");
+	});
+}
 
+//Se limpian los campos marcados anteriormente con errores
+function limpiarCampos(){
+	camposErrores.forEach(function(campo, index) {
+		$(campo).removeClass("is-invalid");
+	});
 
-
-
-
-
-
-/*function validate() {
-	// Inicializar mensaje de error
-	var msg = "";
-
-	//Validar nombre y apellidos
-	var pattern = /^[a-zA-ZñÑ\s\W]+$/;
-	var nombre = document.getElementById("inputNombre");
-	var apellidos = document.getElementById("inputApellidos");
-	if (!pattern.test(nombre.value)) msg += "El nombre únicamente puede contener caracteres no numéricos.\n";
-	if (!pattern.test(apellidos.value)) msg += "Los apellidos únicamente pueden contener letras y espacios.\n";
-
-	//Validar fecha
-	var pattern = /^([0-9]{2})\/([0-9]{2})\/([0-9]{4})$/;
-	var dia = document.getElementById("inputDia").value;
-	var mes = document.getElementById("inputMes").value;
-	var anio = document.getElementById("inputAnio").value;
-	var fecha = dia + '/' + mes + '/' + anio;
-	if (!pattern.test(fecha)) msg += "La fecha de nacimiento es incorrecta, debe seguir el formato (dd/mm/aaaa).\n";
-
-	//Validar teléfono
-	var pattern = /^([0-9]{9})$/;
-	var telefono = document.getElementById("inputTelefono");
-	if (!pattern.test(telefono.value)) msg += "El teléfono es incorrecto, debe estar formado por 9 dígitos.\n ";
-
-	//Validar localidad y país
-	var pattern = /^[a-zA-ZñÑ\s\-\W]+$/;
-	var localidad = document.getElementById("inputLocalidad");
-	var pais = document.getElementById("inputPais");
-	if (!pattern.test(localidad.value)) msg += "La localidad es incorrecta, únicamente puede incluir caracteres no numéricos.\n ";
-	if (!pattern.test(pais.value)) msg += "El país es incorrecto, únicamente puede incluir caracteres no numéricos.\n ";
-
-	//Validar código postal
-	var pattern = /^([0-9]{5})$/;
-	var codigo = document.getElementById("inputcodigo");
-	if (!pattern.test(codigo.value)) msg += "El código postal es incorrecto, debe estar formado por 5 dígitos.\n ";
-
-	if (msg != "") {
-		alert(msg);
-		return false;
-	} else return true;
-}*/
+	camposErrores = [];
+}
